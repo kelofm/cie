02aaf38f915d099bf87f84d4b63a356fad4825fa
matekelemen <kelemen.mate.12@gmail.com>
2025-12-30 08:32:34 +0100

    define blueprint for cell types

---
+++ libraries/fem/packages/numeric/impl/Cell_impl.hpp
@@ -0,0 +1,255 @@
+#pragma once
+
+// help the language server
+#include "packages/numeric/inc/Cell.hpp"
+#include <type_traits>
+
+
+namespace cie::fem {
+
+
+template <unsigned Dim,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+CellBase<Dim,TValue,TSpatialTransform,TData>::CellBase() noexcept
+    : _impl()
+{
+    this->ansatzSpaceID() = 0u;
+    this->id() = 0ul;
+}
+
+
+template <unsigned Dim,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+CellBase<Dim,TValue,TSpatialTransform,TData>::CellBase(VertexID id,
+                                                       AnsatzSpaceID ansatzSpaceID,
+                                                       OrientedAxes<Dimension> axes,
+                                                       RightRef<SpatialTransform> rSpatialTransform) noexcept
+requires std::is_same_v<TData,void>
+    : _impl(ansatzSpaceID, id, axes, std::move(rSpatialTransform), {})
+{
+    this->inverseSpatialTransform() = rSpatialTransform.makeInverse();
+}
+
+
+template <unsigned Dim,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+CellBase<Dim,TValue,TSpatialTransform,TData>::CellBase(VertexID id,
+                                                       AnsatzSpaceID ansatzSpaceID,
+                                                       OrientedAxes<Dimension> axes,
+                                                       RightRef<SpatialTransform> rSpatialTransform,
+                                                       typename VoidSafe<TData,int>::RightRef rData) noexcept
+requires (!std::is_same_v<TData,void>)
+    : _impl(ansatzSpaceID, id, axes, std::move(rSpatialTransform), {}, std::move(rData))
+{
+    this->inverseSpatialTransform() = rSpatialTransform.makeInverse();
+}
+
+
+template <unsigned Dim,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+void CellBase<Dim,TValue,TSpatialTransform,TData>::transform(Ref<const ConstLocalSpan> in,
+                                                             Ref<const GlobalSpan> out) const noexcept {
+    this->spatialTransform().evaluate(
+        Kernel<Dimension,Value>::decay(in),
+        Kernel<Dimension,Value>::decay(out));
+}
+
+
+template <unsigned Dim,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+void CellBase<Dim,TValue,TSpatialTransform,TData>::transform(Ref<const ConstGlobalSpan> in,
+                                                             Ref<const LocalSpan> out) const noexcept {
+    this->inverseSpatialTransform().evaluate(
+        Kernel<Dimension,Value>::decay(in),
+        Kernel<Dimension,Value>::decay(out));
+}
+
+
+template <unsigned Dim,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+typename CellBase<Dim,TValue,TSpatialTransform,TData>::SpatialTransform::Derivative
+CellBase<Dim,TValue,TSpatialTransform,TData>::makeJacobian() const {
+    return this->spatialTransform().makeDerivative();
+}
+
+
+template <unsigned Dimension,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+void io::GraphML::Serializer<CellBase<Dimension,TValue,TSpatialTransform,TData>>::header(Ref<io::GraphML::XMLElement> rElement) const {
+    io::GraphML::XMLElement defaultElement = rElement.addChild("default");
+    Value instance;
+    this->operator()(defaultElement, instance);
+}
+
+
+template <unsigned Dimension,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+void io::GraphML::Serializer<CellBase<Dimension,TValue,TSpatialTransform,TData>>::operator()(Ref<io::GraphML::XMLElement> rElement,
+                                                                                             Ref<const Value> rInstance) const {
+    // Serialize trivial (integral / floating point) members.
+    rElement.addAttribute("id", std::to_string(rInstance.id()));
+    rElement.addAttribute("iAnsatz", std::to_string(rInstance.ansatzSpaceID()));
+
+    // Serialize custom trivial members.
+    {
+        std::stringstream buffer;
+        buffer << rInstance.axes();
+        rElement.addAttribute("axes", buffer.view());
+    }
+
+    // Serialize complex members.
+    io::GraphML::Serializer<typename Value::SpatialTransform>()(
+        rElement,
+        rInstance.spatialTransform());
+
+    // TData is entirely user-defined, so it must either be handled as a
+    // complex member or a trivial one.
+    if constexpr (std::is_integral_v<TData> || std::is_floating_point_v<TData>) {
+        if constexpr (std::is_same_v<TData,bool>) {
+            std::stringstream buffer;
+            buffer << std::boolalpha << rInstance.data();
+            rElement.addAttribute("data", buffer.view());
+        } else if constexpr (std::is_integral_v<TData>) {
+            rElement.addAttribute("data", std::to_string(rInstance.data()));
+        } else {
+            std::stringstream buffer;
+            buffer << std::scientific << std::setprecision(9) << rInstance.data();
+            rElement.addAttribute("data", buffer.view());
+        }
+    } else if constexpr (!std::is_same_v<TData,void>) {
+        io::GraphML::Serializer<TData>()(
+            rElement,
+            rInstance.data());
+    }
+}
+
+
+template <unsigned Dimension,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+void io::GraphML::Deserializer<CellBase<Dimension,TValue,TSpatialTransform,TData>>::onElementBegin(Ptr<void> pThis,
+                                                                                                   std::string_view elementName,
+                                                                                                   std::span<io::GraphML::AttributePair> attributes) {
+    Ref<Deserializer> rThis = *static_cast<Ptr<Deserializer>>(pThis);
+
+    // Deserialize the cell ID.
+    {
+        const auto it = std::find_if(
+            attributes.begin(),
+            attributes.end(),
+            [] (const auto pair) {
+                return pair.first == "id";
+            });
+        char* pEnd;
+        rThis.instance().id() = std::strtoul(it->second.data(), &pEnd, 10);
+    }
+
+    // Deserialize the ansatz space ID.
+    {
+        const auto it = std::find_if(
+            attributes.begin(),
+            attributes.end(),
+            [] (const auto pair) {
+                return pair.first == "iAnsatz";
+            });
+        char* pEnd;
+        rThis.instance().ansatzSpaceID() = std::strtoul(it->second.data(), &pEnd, 10);
+    }
+
+    // Deserialize the cell's orientation in topological space.
+    {
+        const auto it = std::find_if(
+            attributes.begin(),
+            attributes.end(),
+            [] (const auto pair) {
+                return pair.first == "axes";
+            });
+        rThis.instance().axes() = OrientedAxes<Dimension>(it->second);
+    }
+
+    // Deserialize the spatial transformation.
+    {
+        using SubSerializer = io::GraphML::Deserializer<typename Value::SpatialTransform>;
+        rThis.sax().push({
+            SubSerializer::make(rThis.instance().spatialTransform(), rThis.sax(), elementName),
+            SubSerializer::onElementBegin,
+            SubSerializer::onText,
+            SubSerializer::onElementEnd
+        });
+    }
+
+    // Deserialize user data.
+    if constexpr (std::is_integral_v<TData> || std::is_floating_point_v<TData>) {
+        const auto it = std::find_if(
+            attributes.begin(),
+            attributes.end(),
+            [] (const auto pair) {
+                return pair.first == "data";
+            });
+
+        if constexpr (std::is_same_v<TData,bool>) {
+            std::istringstream buffer(it->second);
+            buffer >> std::boolalpha >> rThis.instance().data();
+        } else if constexpr (std::is_integral_v<TData>) {
+            char* pEnd;
+            rThis.instance() = std::strtoul(it->second.data(), &pEnd, 10);
+        } else if constexpr (std::is_floating_point_v<TData>) {
+            std::istringstream buffer(it->second);
+            buffer >> rThis.instance().data();
+        }
+    } else if constexpr (!std::is_same_v<TData,void>) {
+        {
+            using SubSerializer = io::GraphML::Deserializer<TData>;
+            rThis.sax().push({
+                SubSerializer::make(rThis.instance().data(), rThis.sax(), elementName),
+                SubSerializer::onElementBegin,
+                SubSerializer::onText,
+                SubSerializer::onElementEnd
+            });
+        }
+    }
+}
+
+
+template <unsigned Dimension,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+void io::GraphML::Deserializer<CellBase<Dimension,TValue,TSpatialTransform,TData>>::onText(Ptr<void>,
+                                                                                           std::string_view elementName) {
+    CIE_THROW(
+        Exception,
+        "Unexpected text data while parsing a(n) " << elementName << " element in GraphML.")
+}
+
+
+template <unsigned Dimension,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+void io::GraphML::Deserializer<CellBase<Dimension,TValue,TSpatialTransform,TData>>::onElementEnd(Ptr<void> pThis,
+                                                                                                 std::string_view elementName) {
+    Ref<Deserializer> rThis = *static_cast<Ptr<Deserializer>>(pThis);
+    rThis.instance().inverseSpatialTransform() = rThis.instance().spatialTransform.makeInverse();
+    rThis.template release<Deserializer>(&rThis, elementName);
+}
+
+
+} // namespace cie::fem
--- libraries/fem/packages/numeric/inc/Cell.hpp
+++ libraries/fem/packages/numeric/inc/Cell.hpp
@@ -2,20 +2,26 @@
 
 // --- FEM Includes ---
 #include "packages/graph/inc/Graph.hpp"
+#include "packages/graph/inc/OrientedAxes.hpp"
 #include "packages/maths/inc/Expression.hpp"
 #include "packages/utilities/inc/kernel.hpp"
+#include "packages/io/inc/GraphML.hpp"
+#include "packages/io/inc/GraphML_specializations.hpp"
 
 // --- Utility Includes ---
 #include "packages/compile_time/packages/concepts/inc/basic_concepts.hpp"
 
+// --- STL Includes ---
+#include <tuple>
 
-namespace cie::fem::concepts {
+
+namespace cie::fem {
 
 
 template <class T>
-concept Element =
+concept CellLike =
    std::is_same_v<std::remove_cvref_t<decltype(T::Dimension)>,unsigned>
-&& ::cie::concepts::Numeric<typename T::Value>
+&& cie::concepts::Numeric<typename T::Value>
 && requires (const T& rConstInstance,
              std::span<const typename Kernel<T::Dimension,typename T::Value>::LocalCoordinate,T::Dimension> constLocalSpan,
              std::span<typename Kernel<T::Dimension,typename T::Value>::LocalCoordinate,T::Dimension> localSpan,
@@ -23,13 +29,168 @@ concept Element =
              std::span<typename Kernel<T::Dimension,typename T::Value>::GlobalCoordinate,T::Dimension> globalSpan) {
     {rConstInstance.transform(constLocalSpan, globalSpan)}  -> std::same_as<void>;      // <== transform from local to global space
     {rConstInstance.transform(constGlobalSpan, localSpan)}  -> std::same_as<void>;      // <== transform from global to local space
-    {rConstInstance.ID()}                                   -> std::same_as<VertexID>;
+    {rConstInstance.makeJacobian()}                         -> maths::JacobianExpression;
+    {rConstInstance.id()}                                   -> std::same_as<VertexID>;
     {rConstInstance.ansatzSpaceID()}                        -> ::cie::concepts::UnsignedInteger;
-}; // concept Element
+}; // concept Cell
 
 
-template <class T>
-concept Cell = Element<T>;
+template <unsigned Dim,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData = void>
+class CellBase {
+public:
+    constexpr inline static unsigned Dimension = Dim;
+
+    using ConstLocalSpan = std::span<const typename Kernel<Dimension,TValue>::LocalCoordinate,Dimension>;
+
+    using LocalSpan = std::span<typename Kernel<Dimension,TValue>::LocalCoordinate,Dimension>;
+
+    using ConstGlobalSpan = std::span<const typename Kernel<Dimension,TValue>::GlobalCoordinate,Dimension>;
+
+    using GlobalSpan = std::span<typename Kernel<Dimension,TValue>::GlobalCoordinate,Dimension>;
+
+    using AnsatzSpaceID = unsigned short;
+
+    using Value = TValue;
+
+    using Data = TData;
+
+    using SpatialTransform = TSpatialTransform;
+
+    CellBase() noexcept;
+
+    CellBase(VertexID id,
+             AnsatzSpaceID ansatzSpaceID,
+             OrientedAxes<Dimension> axes,
+             RightRef<SpatialTransform> rSpatialTransform) noexcept
+    requires std::is_same_v<TData,void>;
+
+    CellBase(VertexID id,
+             AnsatzSpaceID ansatzSpaceID,
+             OrientedAxes<Dimension> axes,
+             RightRef<SpatialTransform> rSpatialTransform,
+             typename VoidSafe<TData,int>::RightRef rData) noexcept
+    requires (!std::is_same_v<TData,void>);
+
+    void transform(Ref<const ConstLocalSpan> in, Ref<const GlobalSpan> out) const noexcept;
+
+    void transform(Ref<const ConstGlobalSpan> in, Ref<const LocalSpan> out) const noexcept;
+
+    typename SpatialTransform::Derivative makeJacobian() const;
+
+    [[nodiscard]] constexpr AnsatzSpaceID ansatzSpaceID() const noexcept {
+        return std::get<0>(_impl);
+    }
+
+    [[nodiscard]] constexpr VertexID id() const noexcept {
+        return std::get<1>(_impl);
+    }
+
+    [[nodiscard]] constexpr OrientedAxes<Dimension> axes() const noexcept {
+        return std::get<2>(_impl);
+    }
+
+    [[nodiscard]] constexpr typename VoidSafe<const TData>::Ref data() const noexcept
+    requires (!std::is_same_v<TData,void>) {
+        return std::get<5>(_impl);
+    }
+
+    [[nodiscard]] constexpr typename VoidSafe<TData>::Ref data() noexcept
+    requires (!std::is_same_v<TData,void>) {
+        return std::get<5>(_impl);
+    }
+
+private:
+    friend struct io::GraphML::Serializer<CellBase>;
+
+    friend struct io::GraphML::Deserializer<CellBase>;
+
+    [[nodiscard]] constexpr Ref<AnsatzSpaceID> ansatzSpaceID() noexcept {
+        return std::get<0>(_impl);
+    }
+
+    [[nodiscard]] constexpr Ref<VertexID> id() noexcept {
+        return std::get<1>(_impl);
+    }
+
+    [[nodiscard]] constexpr Ref<OrientedAxes<Dimension>> axes() noexcept {
+        return std::get<2>(_impl);
+    }
+
+    [[nodiscard]] constexpr Ref<const TSpatialTransform> spatialTransform() const noexcept {
+        return std::get<3>(_impl);
+    }
+
+    [[nodiscard]] constexpr Ref<TSpatialTransform> spatialTransform() noexcept {
+        return std::get<3>(_impl);
+    }
+
+    [[nodiscard]] constexpr Ref<const typename TSpatialTransform::Inverse> inverseSpatialTransform() const noexcept {
+        return std::get<4>(_impl);
+    }
+
+    [[nodiscard]] constexpr Ref<typename TSpatialTransform::Inverse> inverseSpatialTransform() noexcept {
+        return std::get<4>(_impl);
+    }
+
+    using Impl = std::conditional_t<
+        std::is_same_v<TData,void>,
+        std::tuple<
+            AnsatzSpaceID,
+            VertexID,
+            OrientedAxes<Dimension>,
+            TSpatialTransform,
+            typename TSpatialTransform::Inverse>,
+        std::tuple<
+            AnsatzSpaceID,
+            VertexID,
+            OrientedAxes<Dimension>,
+            TSpatialTransform,
+            typename TSpatialTransform::Inverse,
+            TData>
+    >;
+    Impl _impl;
+}; // class CellBase
 
 
-} // namespace cie::fem::concepts
+template <unsigned Dimension,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+struct io::GraphML::Serializer<CellBase<Dimension,TValue,TSpatialTransform,TData>> {
+    using Value = CellBase<Dimension,TValue,TSpatialTransform,TData>;
+
+    void header(Ref<io::GraphML::XMLElement> rElement) const;
+
+    void operator()(Ref<io::GraphML::XMLElement> rElement,
+                    Ref<const Value> rInstance) const;
+}; // struct io::GraphML::Serializer<CellBase>
+
+
+template <unsigned Dimension,
+          cie::concepts::Numeric TValue,
+          maths::SpatialTransform TSpatialTransform,
+          class TData>
+struct io::GraphML::Deserializer<CellBase<Dimension,TValue,TSpatialTransform,TData>>
+    : public io::GraphML::DeserializerBase<CellBase<Dimension,TValue,TSpatialTransform,TData>>
+{
+    using Value = CellBase<Dimension,TValue,TSpatialTransform,TData>;
+
+    using io::GraphML::DeserializerBase<Value>::DeserializerBase;
+
+    static void onElementBegin(Ptr<void> pThis,
+                               std::string_view elementName,
+                               std::span<io::GraphML::AttributePair> attributes);
+
+    static void onText(Ptr<void>, std::string_view);
+
+    static void onElementEnd(Ptr<void> pThis,
+                             std::string_view elementName);
+}; // struct io::GraphML::Deserializer<CellBase>
+
+
+} // namespace cie::fem
+
+#include "packages/numeric/impl/Cell_impl.hpp"
