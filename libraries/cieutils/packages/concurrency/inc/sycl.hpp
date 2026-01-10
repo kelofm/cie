d2de95cca00ce3c8b11bd292761398955b452c76
matekelemen <kelemen.mate.12@gmail.com>
2026-01-10 17:42:35 +0100

    define dynamic array using sycl shared allocator

--- libraries/cieutils/packages/concurrency/inc/sycl.hpp
+++ libraries/cieutils/packages/concurrency/inc/sycl.hpp
@@ -1,8 +1,28 @@
 #pragma once
 
+// --- External Includes ---
 #ifdef CIE_ENABLE_SYCL
     #ifndef SYCL_LANGUAGE_VERSION
         #define SYCL_LANGUAGE_VERSION 2020
     #endif
     #include <sycl/sycl.hpp>
 #endif
+
+// --- Utility Includes ---
+#include "packages/stl_extension/inc/DynamicArray.hpp"
+
+
+namespace cie {
+
+
+template <class T>
+using DynamicSharedArray =
+#ifdef CIE_ENABLE_SYCL
+    DynamicArray<T,sycl::usm_allocator<T,sycl::usm::alloc::shared>>
+#else
+    DynamicArray<T>
+#endif
+;
+
+
+} // namespace cie
